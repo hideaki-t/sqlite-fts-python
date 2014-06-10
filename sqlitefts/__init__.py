6696ee8637e8edc244255142fa247cc5b7cfe64b
Hideaki Takahashi <mymelo@gmail.com>
2014-06-10 08:35:11 -0400

    added ranking module to symbols in package level

--- sqlitefts/__init__.py
+++ sqlitefts/__init__.py
@@ -1,4 +1,4 @@
 from .tokenizer import Tokenizer, make_tokenizer_module, register_tokenizer
-from . import tokenizer
+from . import tokenizer, ranking
 
-__all__ = ["Tokenizer", "make_tokenizer_module", "register_tokenizer", "tokenizer"]
+__all__ = ["Tokenizer", "make_tokenizer_module", "register_tokenizer", "tokenizer", "ranking"]
