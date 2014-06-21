40dd8f95c845a8b449f4a34501947025aabeac34
Hideaki Takahashi <mymelo@gmail.com>
2014-06-21 17:52:18 -0400

    fixed start/end position of token(in bytes)

--- tests/test_tinysegmenter.py
+++ tests/test_tinysegmenter.py
@@ -17,9 +17,12 @@ class TinySegmenterTokenizer(fts.Tokenizer):
     def tokenize(self, text):
         p = 0
         for t in self.segmenter.tokenize(text):
-            np = text[p:].index(t)
-            yield t, np, len(t.encode('utf-8'))
-            p = np
+            lt = len(t)
+            np = p + text[p:].index(t)
+            start = len(text[:np].encode('utf-8')) + (lt - len(t.lstrip()))
+            txt = t.strip()
+            yield txt, start, start + len(txt.encode('utf-8'))
+            p = np + lt
 
 
 t = TinySegmenterTokenizer()
@@ -33,7 +36,7 @@ def test_make_tokenizer():
 
 
 def test_reginster_tokenizer():
-    name = 'igo'
+    name = 'tinysegmenter'
     c = sqlite3.connect(':memory:')
     tokenizer_module = fts.make_tokenizer_module(t)
     fts.register_tokenizer(c, name, tokenizer_module)
@@ -45,7 +48,7 @@ def test_reginster_tokenizer():
 def test_createtable():
     c = sqlite3.connect(':memory:')
     c.row_factory = sqlite3.Row
-    name = 'igo'
+    name = 'tinysegmenter'
     sql = "CREATE VIRTUAL TABLE fts USING FTS4(tokenize={})".format(name)
     fts.register_tokenizer(c, name, fts.make_tokenizer_module(t))
     c.execute(sql)
@@ -92,3 +95,24 @@ def test_match():
     r = c.execute("SELECT * FROM fts WHERE fts MATCH 'コレは'").fetchall()
     assert len(r) == 0
     c.close()
+
+
+def test_tokenizer_output():
+    name = 'tinysegmenter'
+    with sqlite3.connect(':memory:') as c:
+        fts.register_tokenizer(c, name, fts.make_tokenizer_module(t))
+        c.execute("CREATE VIRTUAL TABLE tok1 USING fts3tokenize({})".format(name))
+        expect = [("This", 0, 4, 0), ("is", 5, 7, 1),
+                  ("a", 8, 9, 2), ("test", 10, 14, 3), ("sentence", 15, 23, 4)]
+        for a, e in zip(c.execute("SELECT token, start, end, position "
+                                  "FROM tok1 WHERE input='This is a test sentence.'"), expect):
+            assert e == a
+
+        s = 'これ は テスト の 文 です'
+        expect = [(None, 0, 0, 0)]
+        for i, txt in enumerate(s.split()):
+            expect.append((txt, expect[-1][2], expect[-1][2] + len(txt.encode('utf-8')), i))
+        expect = expect[1:]
+        for a, e in zip(c.execute("SELECT token, start, end, position "
+                                  "FROM tok1 WHERE input=?", [s.replace(' ', '')]), expect):
+            assert e == a
