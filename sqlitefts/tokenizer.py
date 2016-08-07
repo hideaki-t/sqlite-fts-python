20cd36782f73d49a3511718b1b882644eec6c7fb
Hideaki Takahashi <mymelo@gmail.com>
2016-08-07 16:29:23 -0400

    add hack for pypy
    see #3

--- sqlitefts/tokenizer.py
+++ sqlitefts/tokenizer.py
@@ -5,7 +5,6 @@ a proof of concept implementation of SQLite FTS tokenizers in Python
 from __future__ import print_function, unicode_literals
 
 import sys
-import ctypes
 import struct
 
 from cffi import FFI
@@ -21,7 +20,20 @@ else:
 ffi = FFI()
 ffi.cdef('''
 typedef struct sqlite3 sqlite3;
-int sqlite3_db_config(sqlite3*, int op, ...);
+int sqlite3_db_config(sqlite3 *, int op, ...);
+
+/*
+this structure completely depends on the definition of pysqlite_Connection and
+PyObject_HEAD. this won't work if
+Py_TRACE_REFS is enabled
+AND/OR
+the definition of "sqlite3* db" in pysqlite_Connection is changed/moved
+*/
+typedef struct {
+  size_t ob_refcnt;
+  void *ob_type;
+  sqlite3 *db;
+} PyObject;
 
 typedef struct sqlite3_tokenizer_module sqlite3_tokenizer_module;
 typedef struct sqlite3_tokenizer sqlite3_tokenizer;
@@ -58,22 +70,16 @@ dll = ffi.dlopen("sqlite3")
 
 def f():
     SQLITE_DBCONFIG_ENABLE_FTS3_TOKENIZER = 1004
-    dll = ctypes.CDLL("libsqlite3.so")
-
-    # this structure completely depends on
-    # the definition of pysqlite_Connection and PyObject_HEAD
-    # this won't work if
-    # Py_TRACE_REFS is enabled
-    # AND/OR
-    # the definition of "sqlite3* db" in pysqlite_Connection is changed/moved
-    class PyObject(ctypes.Structure):
-        _fields_ = (("ob_refcnt", ctypes.c_size_t),
-                    ("ob_type", ctypes.c_void_p), ("db", ctypes.c_void_p))
 
     def enable_fts3_tokenizer(c):
-        rc = dll.sqlite3_db_config(
-            ctypes.cast(id(c), ctypes.POINTER(PyObject)).contents.db,
-            SQLITE_DBCONFIG_ENABLE_FTS3_TOKENIZER, 1, 0)
+        db = getattr(c, '_db', None)
+        if db:
+            # pypy's SQLite3 connection has _db using cffi
+            db = ffi.cast('sqlite3*', db)
+        else:
+            db = ffi.cast('PyObject *', id(c)).db
+        rc = dll.sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_FTS3_TOKENIZER,
+                                   ffi.cast('int', 1), ffi.cast('int', 0))
         return rc == 0
 
     return enable_fts3_tokenizer
