1d99157fa6559ba8f7d632cecb396996fb63a515
Hideaki Takahashi <mymelo@gmail.com>
2020-07-01 09:29:05 -0400

    run tests with python 3.8

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="sqlitefts",
-    version="0.6.0",
-    packages=["sqlitefts"],
+    name='sqlitefts',
+    version='0.6.0',
+    packages=['sqlitefts'],
     description='A Python binding for tokenizers of SQLite Full Text Search',
     long_description=open('README.rst').read(),
     url='https://github.com/hideaki-t/sqlite-fts-python/',
@@ -11,9 +11,11 @@ setup(
         'Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
+#        'Programming Language :: Python :: 2.7',
+#        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Database',
