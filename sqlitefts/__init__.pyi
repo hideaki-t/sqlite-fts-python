5e0fc1cb8fe33a95203a17d340e5e4289d62bab9
Hideaki Takahashi <mymelo@gmail.com>
2022-12-17 09:04:31 -0500

    remove stub files

--- sqlitefts/__init__.pyi
+++
@@ -1,6 +0,0 @@
-from . import ranking as ranking
-from . import tokenizer as tokenizer
-from .error import Error as Error
-from .fts3 import Tokenizer as Tokenizer
-from .fts3 import make_tokenizer_module as make_tokenizer_module
-from .fts3 import register_tokenizer as register_tokenizer
--- sqlitefts/ranking.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-def rank(raw_match_info: Any, *weights: Any): ...
-
-simple = rank
-
-def bm25(raw_match_info: Any, *args: Any): ...
